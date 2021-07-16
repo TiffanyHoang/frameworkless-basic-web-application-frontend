fdc0c4f3b7895fd35caf28bf3b7307169e315512
Tiffany Hoang <tiffany.hoang@myob.com>
2021-07-16 10:06:44 +1000

    fetch API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import { getList, setItem } from './services/list'
 
 function App() {
+  const [state, setState] = useState([])
+  const [itemInput, setItemInput] = useState('')
+  const [shouldFetch, setShouldFetch] = useState(true);
+
+  useEffect(() => {
+    if (shouldFetch === true) {
+      getList().then(items => { // get database
+        setState(items)
+      })
+    }
+    setShouldFetch(false)
+  }, [shouldFetch])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setItem(itemInput) // add to database
+      .then(() => {
+        setItemInput("")
+        setShouldFetch(true)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>My Grocery List</h1>
+      <ul>
+        {state.map(item => <li key={item.id}>{item.item}</li>)}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>New Item</p>
+          <input key="here" type="text" onChange={event => setItemInput(event.target.value)} value={itemInput} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
