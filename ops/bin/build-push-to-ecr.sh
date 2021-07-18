6bc7621d9549efd744cffd55c0c9f5b86d6b54af
Tiffany Hoang <tiffany.hoang@myob.com>
2021-07-18 12:52:57 +1000

    changed docker image

--- ops/bin/build-push-to-ecr.sh
+++ ops/bin/build-push-to-ecr.sh
@@ -2,6 +2,8 @@
 set -eou pipefail
 echo '--- :docker: Build app image'
 VERSION=$(git describe --tags --always) 
-docker build -f Dockerfile.prod -t tiffany-frameworkless-basic-web-app-frontend:$VERSION . 
+ECRACC='138666658526.dkr.ecr.ap-southeast-2.amazonaws.com'
+APPIMAGE=$ECRACC/tiffany-frameworkless-basic-web-app-frontend:$VERSION
+docker build -f Dockerfile.prod -t $APPIMAGE . 
 echo '--- :docker: Push app image'
-docker push tiffany-frameworkless-basic-web-app-frontend:$VERSION
+docker push $APPIMAGE
